aa16adc549cdbe6197d9468636a5c43f73ce8a04
G0dsp <g0dsp@protonmail.com>
2023-10-29 13:07:41 +0100

    eye2eye 1.3
    SSL y Virustotal puntuación

--- eye2eye.py
+++ eye2eye.py
@@ -3,8 +3,11 @@ import socket
 import requests
 import builtwith
 import ssl
+import json
+import csv
 
 API_KEY = ''
+API_KEY_VIRUSTOTAL = ''
 
 def obtener_resultados_herramienta(herramienta, parametro):
     url = f"https://api.hackertarget.com/{herramienta}/?q={parametro}&apikey={API_KEY}"
@@ -68,6 +71,38 @@ def imprimir_info_ssl(cert):
     for campo, valor in cert.items():
         print(f"{campo}: {valor}")
 
+def guardar_resultados_en_archivo(resultados, formato):
+    if formato == "txt":
+        with open("resultados.txt", "w") as archivo:
+            archivo.write(resultados)
+    elif formato == "csv":
+        with open("resultados.csv", "w", newline='') as archivo:
+            writer = csv.writer(archivo)
+            for linea in resultados.split('\n'):
+                writer.writerow([linea])
+    elif formato == "json":
+        with open("resultados.json", "w") as archivo:
+            json.dump(resultados, archivo)
+
+def verificar_reputacion_virustotal(dominio):
+    url = f"https://www.virustotal.com/api/v3/domains/{dominio}"
+    headers = {
+        "x-apikey": API_KEY_VIRUSTOTAL
+    }
+    
+    try:
+        respuesta = requests.get(url, headers=headers)
+        respuesta.raise_for_status()
+        datos = respuesta.json()
+        
+        if 'data' in datos and 'attributes' in datos['data'] and 'last_analysis_stats' in datos['data']['attributes']:
+            reputacion = datos['data']['attributes']['last_analysis_stats']
+            return f"Reputación del dominio '{dominio}': {reputacion['malicious']} detectados como maliciosos, {reputacion['suspicious']} sospechosos, {reputacion['harmless']} inofensivos."
+        else:
+            return "No se pudo obtener la reputación del dominio."
+    except requests.exceptions.HTTPError as e:
+        return f"Error al hacer la solicitud a VirusTotal: {str(e)}"
+
 if __name__ == "__main__":
     dominio = input("Ingrese el dominio para buscar información: ")
     
@@ -84,11 +119,14 @@ if __name__ == "__main__":
         "aslookup"
     ]
     
+    resultados_combinados = ""  # Nueva variable para almacenar todos los resultados
+    
     for herramienta in herramientas:
         resultados_herramienta = obtener_resultados_herramienta(herramienta, dominio)
         print(f"\nResultados de {herramienta}:")
-        print(resultados_herramienta)
-
+        print(resultados_herramienta)  # Mostrar resultados en pantalla
+        resultados_combinados += f"\nResultados de {herramienta}:\n{resultados_herramienta}"  # Agregar resultados a la variable
+    
     informacion_dominio = obtener_informacion_dominio(dominio)
     print("\nInformación de dominio:")
     print("\nLa información que aparece abajo tiene que ver con el dominio del que quieres info")
@@ -116,6 +154,20 @@ if __name__ == "__main__":
     print(f"JAVASCRIPT FRAMEWORK --> {js_framework}")
     
     print("\n")
-    # Agregar análisis de SSL
+
     info_ssl = obtener_info_ssl(dominio)
     imprimir_info_ssl(info_ssl)
+
+    reputacion = verificar_reputacion_virustotal(dominio)
+    print(f"\n{reputacion}")
+    
+    # Preguntar al usuario sobre el formato de archivo
+    formato = input("\n¿En qué formato desea guardar los resultados? (txt/csv/json): ").lower()
+
+    # Verificar la entrada del usuario
+    if formato not in ["txt", "csv", "json"]:
+        print("Formato no válido. Se guardarán en formato TXT por defecto.")
+        formato = "txt"
+
+    # Guardar los resultados en el formato elegido
+    guardar_resultados_en_archivo(resultados_combinados, formato)
